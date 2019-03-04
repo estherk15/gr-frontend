8f58f3922faf9b660d9c9040f31bc42f866a2320
github username <github email address>
2019-03-03 22:26:59 -0500

    Styled the signIn

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -15,18 +15,56 @@ class SignIn extends React.Component {
 
   render() {
     return (
-      <div className="level">
+      <nav className="level" id="sign-in">
+
+        <div className="level-left">
+          <div className="level-item">
+            <p className="subtitle is-3">
+              <strong>Good Reader</strong>
+            </p>
+          </div>
+        </div>
+
         <div className="level-right">
-          <strong>Sign In </strong>
-          <form className="level-item"onSubmit={(event) => this.props.signinUser(event, this.state)}>
-            <input className="input" type="text" name="username" placeholder="Username" onChange={this.handleChange}/>
-            <input className="input" type="password" name="password" placeholder="Password" onChange={this.handleChange}/>
-            <input className="button is-primary" type="submit" value="Submit"/>
+          <strong className="level-item">Sign In </strong>
+          <form className="level-item" onSubmit={(event) => this.props.signinUser(event, this.state)}>
+            <div className="level-item">
+              <input className="input" type="text" name="username" placeholder="Username" onChange={this.handleChange}/>
+            </div>
+            <div className="level-item">
+              <input className="input" type="password" name="password" placeholder="Password" onChange={this.handleChange}/>
+            </div>
+            <div className="level-item">
+              <input className="button is-primary" type="submit" value="Submit"/>
+            </div>
           </form>
         </div>
-      </div>
+        
+      </nav>
+
     )
   }
 }
 
 export default SignIn;
+
+
+// <div className="level-right">
+//   <form className='form' className="level-item"onSubmit={(event) => this.props.signinUser(event, this.state)}>
+//     <div className='field'>
+//       <div className='control'>
+//         <input className="input" type="text" name="username" placeholder="Username" onChange={this.handleChange}/>
+//       </div>
+//     </div>
+//     <div className='field'>
+//       <div className='control'>
+//         <input className="input" type="password" name="password" placeholder="Password" onChange={this.handleChange}/>
+//       </div>
+//     </div>
+//     <div className='field'>
+//       <div className='control'>
+//         <input className="button is-primary" type="submit" value="Submit"/>
+//       </div>
+//     </div>
+//   </form>
+// </div>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import reading from '../assets/reading.png'
 
 class SignUp extends React.Component {
   state = {
@@ -14,14 +15,34 @@ class SignUp extends React.Component {
 
   render() {
     return (
-        <div className="signup">
-          New User? Create an account!
-          <form onSubmit={(event)=>this.props.signupUser(event, this.state)}>
-            <input type="text" name="username" placeholder="Username" onChange={this.handleChange}/><br/>
-            <input type="password" name="password" placeholder="Password" onChange={this.handleChange}/>  <br/>
-            <input type="submit" value="Sign up"/>
-          </form>
+      <div className="container" id="sign-up">
+        <div className="level-left">
+          <figure className="image">
+            <img src={reading} className="level-item" alt="Welcome to Good Reader!"/>
+          </figure>
         </div>
+
+        <div className="level-right">
+          <p classname="level-item">
+            <strong className="subtitle is-5">New User? Sign Up Here!</strong>
+          </p>
+
+          <div className="level-item">
+            <form className="form" onSubmit={(event)=>this.props.signupUser(event, this.state)}>
+            <div className="level-item">
+              <input className="input" type="text" name="username" placeholder="Username" onChange={this.handleChange}/>
+            </div>
+            <div className="level-item">
+              <input className="input" type="password" name="password" placeholder="Password" onChange={this.handleChange}/>
+            </div>
+            <div className="level-item">
+              <input className="button is-primary" type="submit" value="Submit"/>
+            </div>
+            </form>
+          </div>
+        </div>
+
+      </div>
     )
   }
 }
